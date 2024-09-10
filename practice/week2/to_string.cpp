63629cd3d76a7fac6889ecdcdd518d7604090186
rkdpwns <yejun99@jnu.ac.kr>
2024-09-10 23:29:56 +0900

    Variable and Data Type Exercises

--- practice/week2/to_string.cpp
+++ practice/week2/to_string.cpp
@@ -7,7 +7,7 @@ int main()
 
     string s1 = "사과";
     string s2;
-    // s2 = s1 + " " + 10 + "개";
+    // s2 = s1 + " " + 10 + "개";, 문자열과 정수를 직접적으로 더할 수 없기 때문
     s2 = s1 + " " + to_string(10) + "개";
     cout << s2 << endl;
 }
